a39d33893fa606ca1284d86ca20a6f87d5995ac9
oliverJv <oliverpereirajv@gmail.com>
2024-10-05 23:15:32 -0600

    Google Auth funcional

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = require('./controller/app'); // Importa tu servidor Express
-
-const port = process.env.PORT || 8085;
-
-// Inicia el servidor
-app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
-});
