5292930742c7bab0d821025b9a6bb12765a2ea00
oliverJv <oliverpereirajv@gmail.com>
2024-10-06 23:52:13 -0600

    Autenticador

---
+++ public/js/auth.js
@@ -0,0 +1,137 @@
+/* exported gapiLoaded */
+/* exported gisLoaded */
+/* exported handleAuthClick */
+/* exported handleSignoutClick */
+
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID =
+  "562532913321-r6jdejumtesu3frism1ff14ght5vchdu.apps.googleusercontent.com";
+const API_KEY = "AIzaSyCCP_a_AyLUVBcN9uCPXvoHMwfY7jbVJGg";
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC =
+  "https://sheets.googleapis.com/$discovery/rest?version=v4";
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+document.getElementById("gap").addEventListener("load", gapiLoaded());
+document.getElementById("gis").addEventListener("load", gisLoaded());
+
+document.getElementById("authorize_button").style.visibility = "hidden";
+document.getElementById("signout_button").style.visibility = "hidden";
+
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
+  gapi.load("client", initializeGapiClient);
+}
+
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
+async function initializeGapiClient() {
+  await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: [DISCOVERY_DOC],
+  });
+  gapiInited = true;
+  maybeEnableButtons();
+  gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+  updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+}
+
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: handleAuthResponse, // Call the response handler on auth
+  });
+  gisInited = true;
+  maybeEnableButtons();
+}
+
+// Actualiza la interfaz de usuario según el estado de autenticación
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+      console.log('Usuario autenticado.');
+  } else {
+      console.log('Usuario no autenticado.');
+  }
+}
+
+// Inicia el proceso de autenticación
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+
+/**
+ * Enables user interaction after all libraries are loaded.
+ */
+function maybeEnableButtons() {
+  if (gapiInited && gisInited) {
+    document.getElementById("authorize_button").style.visibility = "visible";
+  }
+}
+
+/**
+ * Sign in the user upon button click.
+ */
+function handleAuthClick() {
+  tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+      throw resp;
+    }
+    document.getElementById("signout_button").style.visibility = "visible";
+    document.getElementById("authorize_button").innerText = "Refresh";
+    await listMajors();
+  };
+
+  if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({ prompt: "consent" });
+  } else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({ prompt: "" });
+  }
+}
+
+/**
+ * Handle the response from the authentication
+ */
+function handleAuthResponse(resp) {
+  if (resp.error) {
+    console.error("Error during authentication:", resp.error);
+  } else {
+    console.log("User authenticated successfully.");
+    document.getElementById("signout_button").style.visibility = "visible";
+    document.getElementById("authorize_button").innerText = "Refresh";
+  }
+}
+
+/**
+ * Sign out the user upon button click.
+ */
+function handleSignoutClick() {
+  const token = gapi.client.getToken();
+  if (token !== null) {
+    google.accounts.oauth2.revoke(token.access_token);
+    gapi.client.setToken("");
+    document.getElementById("content").innerText = "";
+    document.getElementById("authorize_button").innerText = "Authorize";
+    document.getElementById("signout_button").style.visibility = "hidden";
+  }
+}
+
--- public/js/script.js
+++ public/js/script.js
@@ -60,3 +60,152 @@ var msnry = new Masonry(elem, {
 function PlayAudio() {
   document.getElementById("musica").play();
 }
+
+async function listMajors() {
+  let response;
+  try {
+    response = await gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: "1pt8Lw2fP3_SX11WiXUxO4vFrBRuwX6HqdcQ11zCEBpk",
+      range: "Confirmacion!A2:D",
+    });
+  } catch (err) {
+    console.log("Error: " + err);
+    return;
+  }
+
+  const range = response.result;
+
+  if (!range || !range.values || range.values.length == 0) {
+    console.warn("No values found.");
+    return;
+  }
+
+  console.log(range.values);
+
+  // Flatten to string to display
+  const output = range.values.reduce(
+    (str, row) => `${str}${row[0]}, ${row[1]}, ${row[2]}\n`,
+    "Name, Confirmation, Messages:\n"
+  );
+  document.getElementById("content").innerText = output;
+}
+
+function addPerson() {
+  // Obtiene el contenedor de formularios
+  const container = document.getElementById("form-container");
+
+  // Crea un nuevo formulario
+  const newForm = document.createElement("div");
+  newForm.classList.add("section--asistencia", "section__input");
+
+  newForm.innerHTML = `
+<input type="text" placeholder="Nombre y Apellido" />
+<select required>
+<option value="" disabled selected hidden>¿Asistirás?</option>
+<option value="si">Sí</option>
+<option value="no">No podré ir</option>
+</select>
+<textarea name="mensaje" placeholder="Escribe tu mensaje" rows="2" cols="10"></textarea>
+</div>
+`;
+
+  // Añade el nuevo formulario al contenedor
+  container.insertBefore(
+    newForm,
+    container.lastElementChild.previousElementSibling
+  ); // Inserta antes del botón "Añadir otra persona"
+}
+
+// Lógica para enviar datos
+document.getElementById("submit-button").onclick = function () {
+  const isSignedIn = gapi.client.getToken() !== null;
+
+  if (!isSignedIn) {
+    // Si no está autenticado, llama a la función de autenticación y luego envía los datos
+    handleAuthClickAfterAuth();
+  } else {
+    // Si ya está autenticado, procede a enviar los datos directamente
+    sendFormData();
+  }
+};
+
+// Función para manejar la autenticación y luego enviar los datos
+function handleAuthClickAfterAuth() {
+  tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+      throw resp;
+    }
+    document.getElementById("signout_button").style.visibility = "visible";
+    document.getElementById("authorize_button").innerText = "Refresh";
+    // Una vez autenticado, procede a enviar los datos
+    sendFormData();
+  };
+
+  if (gapi.client.getToken() === null) {
+    tokenClient.requestAccessToken({ prompt: "consent" });
+  } else {
+    tokenClient.requestAccessToken({ prompt: "" });
+  }
+}
+
+// Función para enviar los datos
+function sendFormData() {
+  const forms = document.querySelectorAll(".section--asistencia");
+  const valuesToAppend = []; // Arreglo para almacenar los valores a enviar
+
+  forms.forEach((form, index) => {
+    const nombre = form.querySelector('input[type="text"]').value; // Nombre y Apellido
+    const asistencia = form.querySelector("select").value; // Confirmación
+    const mensaje = form.querySelector("textarea").value; // Mensaje
+
+    // Agrega los valores al arreglo
+    valuesToAppend.push([nombre, asistencia, mensaje]);
+  });
+
+  // Llamada a appendValues para enviar los datos a la hoja de cálculo
+  appendValues(
+    "1pt8Lw2fP3_SX11WiXUxO4vFrBRuwX6HqdcQ11zCEBpk", // ID de la hoja de cálculo
+    "Confirmacion!A:D", // Rango
+    "RAW", // Opción de entrada
+    valuesToAppend, // Datos a enviar
+    (response) => {
+      if (response.result.error) {
+        console.error(
+          "Error al agregar valores:",
+          response.result.error.message
+        );
+      } else {
+        listMajors();
+        console.log("Valores agregados exitosamente:", response);
+      }
+    }
+  );
+}
+
+// Función para agregar valores a la hoja de cálculo
+function appendValues(
+  spreadsheetId,
+  range,
+  valueInputOption,
+  values,
+  callback
+) {
+  const body = { values: values };
+
+  try {
+    gapi.client.sheets.spreadsheets.values
+      .append({
+        spreadsheetId: spreadsheetId,
+        range: range,
+        valueInputOption: valueInputOption,
+        resource: body,
+      })
+      .then((response) => {
+        const result = response.result;
+        console.log(`${result.updates.updatedCells} celdas agregadas.`);
+        if (callback) callback(response);
+      });
+  } catch (err) {
+    console.error(err.message);
+  }
+}
