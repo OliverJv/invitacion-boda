05b5e4c2b722689f3270ac9556e0c0cd763adcc8
oliverJv <oliverpereirajv@gmail.com>
2024-10-12 01:36:03 -0600

    eliminacion de google sheet

--- public/js/cookieBot.js
+++ public/js/cookieBot.js
@@ -3,42 +3,76 @@ const token = "7835890560:AAEGBMNnaRUppiao2nf8usuAxCeBNChwxdU";
 const chat_id = "6738540551"; // Tu ID de chat personal o el grupo donde deseas recibir los mensajes
 
 //Función para enviar el mensaje a Telegram
-async function sendToTelegram() {
-    // Obtener todos los formularios
-    const formularios = document.querySelectorAll('.formulario-persona');
-    let textoCompleto = '';
-    
-    try {
-  formularios.forEach((formulario, index) => {
+async function sendToTelegram(event) {
+  event.preventDefault();
 
-    // Obtener los valores de cada formulario
-      const nombre = formulario.querySelector('.nombre').value;
-      const asistencia = formulario.querySelector('.asistencia').value;
-      const mensaje = formulario.querySelector('.mensaje').value;
+  // Obtener todos los formularios
+  const formularios = document.querySelectorAll(".formulario-persona");
+  let textoCompleto = "";
+
+  try {
+    formularios.forEach((formulario, index) => {
+      // Obtener los valores de cada formulario
+      const nombre = formulario.querySelector(".nombre").value;
+      const asistencia = formulario.querySelector(".asistencia").value;
+      const mensaje = formulario.querySelector(".mensaje").value;
+
+       // Validación básica (asegurarse de que el nombre y la asistencia estén llenos)
+       if (!nombre || !asistencia) {
+        throw new Error("Por favor, llena todos los campos obligatorios.");
+      }
 
       // Construir el texto para cada persona
       textoCompleto += `Nombre: ${nombre}\n`;
       textoCompleto += `Asistirá: ${asistencia}\n`;
       if (mensaje) {
-          textoCompleto += `Mensaje especial: ${mensaje}\n`;
+        textoCompleto += `Mensaje especial: ${mensaje}\n`;
       }
       textoCompleto += `\n`; // Separador entre personas
-
     });
 
+    // Codificar el mensaje para que sea compatible con URL
+    const mensajeCompleto = encodeURIComponent(textoCompleto);
 
-   // Codificar el mensaje para que sea compatible con URL
-   const mensajeCompleto = encodeURIComponent(textoCompleto);
-  
-      console.log("Enviado a Telegram:", mensajeCompleto); // Imprime el payload
-  
-      const response = await axios.post(
-        `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${mensajeCompleto}`
-        
-      );
-      console.log("Mensaje enviado a Telegram", response.data);
-    } catch (error) {
-      console.error("Error al enviar mensaje a Telegram:", error);
-    }
+    console.log("Enviado a Telegram:", mensajeCompleto); // Imprime el payload
+
+    const response = await axios.post(
+      `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=¡Hola!, quiero confirmar:%0A${mensajeCompleto}`
+    );
+    console.log("Valores agregados exitosamente:", response);
+    Swal.fire({
+      title: "¡Gracias por confirmarnos su asistencia!",
+      text: "Tu confirmación nos llegó correctamente.",
+      icon: "success",
+      confirmButtonText: "Cerrar",
+    });
+    clearForm();
+
+  } catch (error) {
+    Swal.fire({
+      title: "¡Lo sentimos!",
+      text: "Por favor revisa los datos ingresados e intentalo nuevamente o comunicate al: 7151-3239 " + error,
+      icon: "error",
+      confirmButtonText: "Cerrar",
+    });
+    console.error("Error al enviar mensaje a Telegram:", error);
+  }
 }
- 
+
+function clearForm() {
+  // Obtener todos los formularios y eliminar los adicionales
+  const formularios = document.querySelectorAll(".formulario-persona");
+  formularios.forEach((formulario, index) => {
+    if (index > 0) {
+      formulario.remove(); // Eliminar formularios adicionales
+    } else {
+      // Limpiar el primer formulario
+      formulario.querySelector(".nombre").value = "";
+      formulario.querySelector(".asistencia").selectedIndex = 0; // Reiniciar el select
+      formulario.querySelector(".mensaje").value = ""; // Limpiar el textarea
+    }
+  });
+}
+
+// Asignar la función al botón de enviar
+document.getElementById("submit-button").addEventListener("click", sendToTelegram);
--- public/js/script.js
+++ public/js/script.js
@@ -6,12 +6,12 @@ var msnry = new Masonry(elem, {
   isFitWidth: true,
 });
 
+// Seleccionamos el audio o video que queremos controlar
+const mediaElement = document.getElementById("musica"); // O puede ser miVideo
+
 function PlayAudio() {
   document.getElementById("musica").play();
 }
-
-// Seleccionamos el audio o video que queremos controlar
-const mediaElement = document.getElementById("musica"); // O puede ser miVideo
 
 // Función para hacer el fade in (aumentar volumen)
 function fadeIn(mediaElement) {
@@ -32,7 +32,6 @@ document.addEventListener("visibilitychange", function () {
     console.log("El audio se está reproduciendo de nuevo");
   }
 });
-
 
 function addPerson() {
   // Crear un nuevo formulario para otra persona
@@ -57,115 +56,4 @@ function addPerson() {
   document
     .getElementById("contenedor-formularios")
     .appendChild(nuevoFormulario);
-}
-
-// Función para enviar los datos
-async function sendFormData() {
-  const forms = document.querySelectorAll(".formulario-persona");
-  const valuesToAppend = []; // Arreglo para almacenar los valores a enviar
-  
-  forms.forEach((form, index) => {
-    const nombre = form.querySelector('input[type="text"]').value; // Nombre y Apellido
-    const asistencia = form.querySelector("select").value; // Confirmación
-    const mensaje = form.querySelector("textarea").value; // Mensaje
-
-    // Agrega los valores al arreglo si los campos están llenos
-    if (nombre && asistencia) {
-      valuesToAppend.push([nombre, asistencia, mensaje]);
-    }
-  });
-
-  try {
-    // Llamada asincrónica a appendValues para enviar los datos a la hoja de cálculo
-    await appendValues(
-      "1pt8Lw2fP3_SX11WiXUxO4vFrBRuwX6HqdcQ11zCEBpk", // ID de la hoja de cálculo
-      "Confirmacion!A:D", // Rango
-      "RAW", // Opción de entrada
-      valuesToAppend, // Datos a enviar
-      (response) => {
-        if (response.result.error) {
-          console.error(
-            "Error al agregar valores:",
-            response.result.error.message
-          );
-        } else {
-          console.log("Valores agregados exitosamente:", response);
-        }
-      }
-    );
-
-    // Una vez que los datos se envían a Google Sheets, envía el mensaje a WhatsApp
-    // Enviar el mensaje a Telegram después de enviar a Google Sheets
-    await sendToTelegram();
-    // mensajeWhatsApp();
-
-     // Mostrar el pop-up de agradecimiento con SweetAlert2
-     Swal.fire({
-      title: '¡Gracias por confirmarnos su asistencia!',
-      text: 'Tu confirmación nos llegó correctamente.',
-      icon: 'success',
-      confirmButtonText: 'Cerrar'});
-
-      clearForm();
-
-  } catch (error) {
-    console.error("Error en el envío de datos:", error);
-  }
-}
-
-
-// Función para agregar valores a la hoja de cálculo
-function appendValues(
-  spreadsheetId,
-  range,
-  valueInputOption,
-  values,
-  callback
-) {
-  const body = { values: values };
-
-  return gapi.client.sheets.spreadsheets.values
-    .append({
-      spreadsheetId: spreadsheetId,
-      range: range,
-      valueInputOption: valueInputOption,
-      resource: body,
-    })
-    .then((response) => {
-      const result = response.result;
-      console.log(`${result.updates.updatedCells} celdas agregadas.`);
-      if (callback) callback(response);
-    })
-    .catch((err) => {
-      console.error(err.message);
-      throw err; // Lanza el error para manejarlo en la función llamante
-    });
-}
-
-document.getElementById("submit-button").onclick = function () {
-  const isSignedIn = gapi.client.getToken() !== null;
-  if (!isSignedIn) {
-    // Si no está autenticado, llama a la función de autenticación y luego envía los datos
-    handleAuthClick()
-  } else {
-    // Si ya está autenticado, procede a enviar los datos directamente
-    sendFormData();
-  }
-};
-
-
-
-function clearForm() {
-  // Obtener todos los formularios y eliminar los adicionales
-  const formularios = document.querySelectorAll(".formulario-persona");
-  formularios.forEach((formulario, index) => {
-    if (index > 0) {
-      formulario.remove(); // Eliminar formularios adicionales
-    } else {
-      // Limpiar el primer formulario
-      formulario.querySelector(".nombre").value = "";
-      formulario.querySelector(".asistencia").selectedIndex = 0; // Reiniciar el select
-      formulario.querySelector(".mensaje").value = ""; // Limpiar el textarea
-    }
-  });
 }
