240c6023a0bab14b86730a780a4d94db57e0cf77
oliverJv <oliverpereirajv@gmail.com>
2024-10-16 18:53:06 -0600

    Correccion diseño

--- public/js/cuenta-regresiva.js
+++ public/js/cuenta-regresiva.js
@@ -2,8 +2,8 @@ simplyCountdown("#cuenta", {
   year: 2024, // required
   month: 12, // required
   day: 8, // required
-  hours: 23, // Default is 0 [0-23] integer
-  minutes: 59, // Default is 0 [0-59] integer
+  hours: 9, // Default is 0 [0-23] integer
+  minutes: 0, // Default is 0 [0-59] integer
   seconds: 0, // Default is 0 [0-59] integer
   words: {
     //words displayed into the countdown
